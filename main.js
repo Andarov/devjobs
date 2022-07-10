e13d944937b22c1f83944d9bfdadde7e34e7fd2b
Andarov <bekturandarob678@gmail.com>
2022-07-10 09:04:29 +0500

    Add modal

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ elDay.addEventListener("click", function () {
 elNight.addEventListener("click", function () {
     document.body.classList.add("dark-mode");
 });
+
+const elBtnFilter = document.querySelector(".js-filter");
+const elBtnExit = document.querySelector(".js-exit");
+const elModal = document.querySelector(".modal");
+
+if (elBtnFilter) {
+    elBtnFilter.addEventListener("click", function () {
+        elModal.classList.add("modal-open");
+    });
+}
+
+if (elBtnExit) {
+    elBtnExit.addEventListener("click", function () {
+        elModal.classList.remove("modal-open");
+    });
+}
